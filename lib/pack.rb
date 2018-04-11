ab4be164c20c606721ee73aa7036df1ee010e921
Javi Fontan <jfontan@gmail.com>
2018-04-11 18:33:44 +0200

    Basic borges pack executor

---
+++ lib/pack.rb
@@ -0,0 +1,40 @@
+require 'repositories'
+require 'executor'
+require 'tmpdir'
+
+# Runs borges pack tests and check the results
+class Pack
+  def initialize(ops = {})
+    @main_conf = ops[:conf]
+    @conf = @main_conf['tests']['pack']
+    @repos = ops[:repos]
+  end
+
+  def run
+    results = {}
+
+    @conf.each do |name, resources|
+      log("Pack test: #{name}")
+      begin
+        tmpdir = Dir.mktmpdir('test-pack')
+        list = File.join(tmpdir, 'list')
+        open(list, 'w') do |f|
+          f.write(@repos.local_url(name))
+        end
+
+        exec = Executor.new('borges', 'pack',
+                            "--file=#{list}", "--to=#{tmpdir}")
+        res = exec.run
+
+        results[name] = {
+          status: res[0],
+          log: res[1]
+        }
+      ensure
+        FileUtils.remove_entry(tmpdir)
+      end
+    end
+
+    results
+  end
+end
